83d023322fe095b8e9b90fd52aa6e27992fceae6
nicruireq <nicruireq.academic@outlook.com>
2018-10-21 16:22:52 +0200

    creado prueba.cpp ejemplo de creacion de hilos en c++

---
+++ prueba.cpp
@@ -0,0 +1,65 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+static int var {0};
+
+void mifunc()
+{
+    ++var;
+}
+
+class thread_guard
+{
+	thread& t;
+public:
+	explicit thread_guard(thread& t_)
+		t(t_) {}
+	~thread_guard()
+	{
+		if (t.joinable())
+			t.join();
+	}
+	thread_guard(thread_guard const&) =delete;
+	thread_guard& operator=(thread_guard const&) =delete;
+};
+
+class prueba
+{
+public:
+    prueba() : v(var) {}
+    prueba(int& _v) : v(_v) {}
+    void operator()()
+    { 
+        cout << "ejecutando hilo...\n";
+		cout << "v antes de incrementar = "<< v <<"\n";
+        for(int i=0;i<1000000;++i) ++v; 
+		cout << "v despues de incrementar = "<< v <<"\n";
+    }
+private:
+    int& v;
+};
+
+void lanzahilos()
+{
+    cout << "comenzando lanzahilos()...\n";
+    thread t1{mifunc};
+    //thread t2(prueba());      //lo interpreta como declaracion de funcion con un parametro de tipo puntero a funcion
+    //thread t2( (prueba()) );  //elimina la ambiguedad
+    thread t2{prueba()};        // con inicializador uniforme eliminamos la ambiguedad
+    for (int i=0; i<100;++i)
+        cout << "lanzahilos() esta haciendo cosas...\n";
+    t1.join();
+    t2.join();
+    cout << "terminando lanzahilos()...\n";
+}
+
+int main()
+{
+    cout << "comenzando main...\n";
+    lanzahilos();
+    cout << "terminando main...\n";
+}
